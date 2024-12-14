9490125837ac59d08ba8df8faece18e29bd709ea
Will Anderson <143653080+Mushito@users.noreply.github.com>
2024-12-14 12:34:19 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,112 +4,126 @@ import time
 from machine import PWM, Pin
 from ir_rx.nec import NEC_8
 from ir_rx.print_error import print_error
-import seesaw
-from ir_tx.nec import NEC
-
-ir_pin = Pin(17, Pin.IN, Pin.PULL_UP)
 
 RFA = Pin(4, Pin.IN, Pin.PULL_UP)  # RF Go
 RFB = Pin(5, Pin.IN, Pin.PULL_UP)  # RF Reverse
 RFC = Pin(6, Pin.IN, Pin.PULL_UP)  # RF Turn Right
 RFD = Pin(7, Pin.IN, Pin.PULL_UP)  # RF Turn Left
 
-pwm_rate = 2000
-ain1_ph = Pin(12, Pin.OUT)
-ain2_en = PWM(Pin(13), freq=pwm_rate, duty_u16=0) 
-bin1_ph = Pin(9, Pin.OUT)
-bin2_en = PWM(Pin(8), freq=pwm_rate, duty_u16=0)
-pwm = 32000
+pwm = 16000
+turn_pwm = 10000
+ain1_ph = Pin(14, Pin.OUT)
+ain2_en = PWM(Pin(15), freq=pwm, duty_u16=0)
 
-i2c = machine.I2C(0, scl=Pin(17), sda=Pin(16))
-seesaw_device = seesaw.Seesaw(i2c, addr=0x50)
-tx_pin = Pin(18, Pin.OUT, value=0)
-device_addr = 0x01
-transmitter = NEC(tx_pin)
+bin1_ph = Pin(12, Pin.OUT)
+bin2_en = PWM(Pin(13), freq=pwm, duty_u16=0)
 
-BUTTON_SELECT = 0
-BUTTONS_MASK = 1 << BUTTON_SELECT
+led = Pin("LED", Pin.OUT)
+led.toggle()
 
-commands = {
-    0x10: "Motor CounterClockwise",
-    0x20: "Stop Motors",
-    0x30: "Motor Clockwise",
-    0x40: "Turn Left",
-    0x50: "Turn Right",
-}
+ir_pin = Pin(18, Pin.IN, Pin.PULL_UP)
 
-RF_COMMANDS = {
-    RFA: 0x10,
-    RFB: 0x20,
-    RFC: 0x40,
-    RFD: 0x30,
-}
+RF_ON = False
 
 def ir_callback(data, addr, _):
+    global RF_ON
     print(f"Received IR command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
-    control_motors(data)
-
-def control_motors(command):
-    if command == 0x10:  # motor CounterClockwise
-        print("Motor CounterClockwise")
-        ain1_ph.low()
-        ain2_en.duty_u16(pwm)
-        bin1_ph.high()
-        bin2_en.duty_u16(pwm)
-    elif command == 0x20:  # stop motors
-        print("Motor OFF")
-        ain1_ph.low()
-        ain2_en.duty_u16(0)
-        bin1_ph.low()
-        bin2_en.duty_u16(0)
-    elif command == 0x30:  # motor Clockwise
-        print("Motor Clockwise")
-        ain1_ph.high()
-        bin1_ph.low()
-        ain2_en.duty_u16(pwm)
-        bin2_en.duty_u16(pwm)
-    elif command == 0x40: # Turn Left
-        print ("Turning Left")
-        ain1_ph.high()
-        bin1_ph.high()
-        ain2_en.duty_u16(pwm)
-        bin2_en.duty_u16(pwm)
-    elif command == 0x50: # Turn Right
-        print ("Turning Right")
-        ain1_ph.low()
-        bin1_ph.low()
-        ain2_en.duty_u16(pwm)
-        bin2_en.duty_u16(pwm)
+    if data == 0x60:  # Toggle RF mode
+        led.toggle()
+        RF_ON = not RF_ON
+        print("RF MODE:", "ON" if RF_ON else "OFF")
+    elif data == 0x70:
+        pwm = 32000
+    elif data == 0x80:
+        pwm = 8000
     else:
-        print("Unknown command")
+        control_motors(data)
+
+def ramp_motors_straight(motor1, motor2):
+    for i in range(0, pwm, 1000):
+        motor1.duty_u16(i)
+        motor2.duty_u16(i)
+        time.sleep(0.01)
+
+def ramp_motors_turn(motor1, motor2):
+    for i in range(0, turn_pwm, 1000):
+        motor1.duty_u16(i)
+        motor2.duty_u16(i)
+        time.sleep(0.01)
 
 ir_receiver = NEC_8(ir_pin, callback=ir_callback)
 ir_receiver.error_function(print_error)
 
-def setup_buttons():
-    seesaw_device.pin_mode_bulk(BUTTONS_MASK, seesaw_device.INPUT_PULLUP)
+commands = {
+    0x10: "Right",
+    0x20: "Backwards",
+    0x30: "Foward",
+    0x40: "Left",
+    0x50: "Stop Motors",
+}
 
-def read_buttons():
-    return seesaw_device.digital_read_bulk(BUTTONS_MASK)
+RF_COMMANDS = {
+    RFA: 0x10,
+    RFB: 0x30,
+    RFC: 0x40,
+    RFD: 0x50,
+}
 
-RF_ON = False
-setup_buttons()
+def control_motors(command):
+    if command == 0x10:  # motor CounterClockwise
+        ain1_ph.low()
+        bin1_ph.high()
+        ramp_motors_turn(ain2_en, bin2_en)
+    elif command == 0x50:  # stop motors
+        print("Motor OFF")
+        motors_off()
+    elif command == 0x30:  # motor Clockwise
+        ain1_ph.high()
+        bin1_ph.low()
+        ramp_motors_straight(ain2_en, bin2_en)
+    elif command == 0x40:  # Turn Left
+        ain1_ph.high()
+        bin1_ph.high()
+        ramp_motors_turn(ain2_en, bin2_en)
+    elif command == 0x20:  # straight
+        ain1_ph.low()
+        bin1_ph.low()
+        ramp_motors_straight(ain2_en, bin2_en)
+    else:
+        print("Unknown command")
+
+def motors_off():
+    ain1_ph.low()
+    bin1_ph.low()
+    ain2_en.duty_u16(0)
+    bin2_en.duty_u16(0)
 
 while True:
-    # Check SELECT button to toggle between RF and IR
-    current_buttons = read_buttons()
-    if not current_buttons & (1 << BUTTON_SELECT):
-        RF_ON = not RF_ON
-        print("RF MODE:", "ON" if RF_ON else "OFF")
-        time.sleep(0.3)
-
     if RF_ON:
-        for pin, command in RF_COMMANDS.items():
-            if not pin.value():
-                print(f"RF COMMAND {commands[command]} EXECUTED")
-                control_motors(command)
-                time.sleep(0.3)
+        active_command = None
+        if RFA.value():  # Button pressed (value = 0)
+            active_command = 0x10
+            control_motors(0x10)
+            time.sleep(0.5)
+            active_command = None
+            motors_off()
+        elif RFB.value():  # Reverse
+            active_command = 0x30
+            control_motors(0x30)
+            time.sleep(0.5)
+            active_command = None
+            motors_off()
+        elif RFC.value():  # Turn Right
+            active_command = 0x40
+            control_motors(0x40)
+            time.sleep(0.5)
+            active_command = None
+            motors_off()
+        elif RFD.value():  # Stop Motors
+            time.sleep(0.5)
+            active_command = None
+            motors_off()
     else:
         pass
+    time.sleep(0.05)
 
-    time.sleep(0.1)
